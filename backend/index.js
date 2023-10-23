1b8ccdec75e65fc648b333d90e2eceb868303d2f
tamatee <teeracharoenkitisap@gmail.com>
2023-10-23 23:44:12 +0700

    [tamate -> tamate imprement post method to this code]

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,32 @@ router.get('/formats/gif', async (req, res) => {
     }
 });
 
+router.post('/upload', async (req, res) => {
+    try {
+        const imageUrl = 'http://example.com/path/to/your/image.jpg'; // Replace with the actual URL of the image
+        const imageResponse = await axios.get(imageUrl, {
+            responseType: 'arraybuffer',
+        });
+        const formData = new FormData();
+        formData.append('source_file', imageResponse.data, {
+            filename: 'image.jpg',
+        });
+
+        const response = await axios.post('https://sandbox.zamzar.com/v1/jobs', formData, {
+            auth: {
+                username: apiKey,
+                password: ''
+            },
+            headers: formData.getHeaders()
+        });
+        console.log('SUCCESS! File uploaded:', response.data);
+        res.send(response.data);
+    } catch (error) {
+        console.error('Unable to upload file', error);
+        res.status(500).send(error);
+    }
+});
+
 app.use(apiRoot, router);
 
 app.listen(port, () => {
